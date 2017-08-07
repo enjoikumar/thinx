df5efc3f9c61662b8a85b9f195c83b01ca86db10
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-08-07 15:16:55 -0400

    added notes for later

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
 	console.log('hello');
 })
+
+//Left text and right text fields are sticky
+
+//for mobile create a function that is enabled when browser is a certain height
+
+// if($(window).width() >= 1920){
+//     $('.br').find('br').remove();
+//   }
+
+// So when browser width is >= 414 invoke flickity for images
+
+// don't know about enlarging th image except for modal
+// but click function to make it bigger and make everything except images display none
+// also cursor is a magnifying glass + and - respectively
+// cant find counter code, do click add class, click remove class to enlarge and stuff 
+// challenge would be to make the image change size as well as responsiveness.
+
+// positioning of images in the bottom. Are they in a cetain format, size, layout etc. 
